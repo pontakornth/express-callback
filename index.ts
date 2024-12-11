3ecebe805688e742f861cfaa71b262483f9ac39b
pontakornth <pontakorn61@gmail.com>
2024-12-11 12:23:45 +0700

    feat

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
-import express from 'express'
-import type { Request, Response } from 'express'
+import express from "express";
+import type { Request, Response } from "express";
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
 app.use("/callback", (req: Request, res: Response) => {
-	console.log(req.body)
-	res.json({status: 'ok'})
-})
+  console.log(req.body);
+  res.json({ status: "ok" });
+});
 
 app.listen(4888, () => {
-							console.log("listening on port 4888")
-})
+  console.log("listening on port 4888");
+});
